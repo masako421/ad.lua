b3376e22581d35ae14b1e2044f6d48644b6d26e1
masako421 <tanakamasatyo@gmail.com>
2025-10-28 20:31:30 +0900

    Update aa.lua

--- aa.lua
+++ aa.lua
@@ -1,6 +1,32 @@
-local Money_to_add = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-local Money_to_remove = 0
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LP = Players.LocalPlayer
 
-local trash_multiplier = 100000
-game:GetService("ReplicatedStorage"):WaitForChild("Purchase_Cairo_Crate"):FireServer(-Money_to_add/trash_multiplier)
-game:GetService("ReplicatedStorage"):WaitForChild("Purchase_Cairo_Crate"):FireServer(Money_to_remove/trash_multiplier)
+local food = {}
+for _, part in ipairs(workspace:GetDescendants()) do
+    if part.Name == "new_food" then
+        table.insert(food, part)
+    end
+end
+
+local Index = 1
+
+local function yess()
+    local Stop = math.min(Index + 25 - 1, #food)
+        
+    for i = Index, Stop do
+            local food = food[i]
+        if LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") then
+            local hrp = LP.Character.HumanoidRootPart
+            food.CanCollide = false
+            food.Transparency = 1
+            food.CFrame = hrp.CFrame + Vector3.new(0, 0, 0)
+        end
+    end
+
+    Index = Stop + 1
+    if Index <= #food then
+        task.delay(0.1, yess)
+    end
+end
+yess()
