f4122b22ccc64e0ad441896807444ead2380470b
masako421 <tanakamasatyo@gmail.com>
2025-10-28 20:25:38 +0900

    Update aa.lua

--- aa.lua
+++ aa.lua
@@ -1,1 +1,6 @@
-game:GetService("ReplicatedStorage").AdminRankEvent:FireServer()
+local Money_to_add = 1000
+local Money_to_remove = 0
+
+local trash_multiplier = 100000
+game:GetService("ReplicatedStorage"):WaitForChild("Purchase_Cairo_Crate"):FireServer(-Money_to_add/trash_multiplier)
+game:GetService("ReplicatedStorage"):WaitForChild("Purchase_Cairo_Crate"):FireServer(Money_to_remove/trash_multiplier)
