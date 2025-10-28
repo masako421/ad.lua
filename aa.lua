53dcb0bb7b49215639939861b8f92838564672ba
masako421 <tanakamasatyo@gmail.com>
2025-10-28 20:35:14 +0900

    Update aa.lua

--- aa.lua
+++ aa.lua
@@ -1,32 +1,25 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local LP = Players.LocalPlayer
-
+local lp = game.Players.LocalPlayer
 local food = {}
-for _, part in ipairs(workspace:GetDescendants()) do
-    if part.Name == "new_food" then
-        table.insert(food, part)
-    end
+for _, v in ipairs(workspace:GetDescendants()) do
+	if v.Name == "new_food" then
+		table.insert(food, v)
+	end
 end
-
-local Index = 1
-
-local function yess()
-    local Stop = math.min(Index + 25 - 1, #food)
-        
-    for i = Index, Stop do
-            local food = food[i]
-        if LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") then
-            local hrp = LP.Character.HumanoidRootPart
-            food.CanCollide = false
-            food.Transparency = 1
-            food.CFrame = hrp.CFrame + Vector3.new(0, 0, 0)
-        end
-    end
-
-    Index = Stop + 1
-    if Index <= #food then
-        task.delay(0.1, yess)
-    end
+local i = 1
+local function move()
+	local stop = math.min(i + 25, #food)
+	for n = i, stop do
+		local f = food[n]
+		if f and lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
+			local hrp = lp.Character.HumanoidRootPart
+			f.CanCollide = false
+			f.Transparency = 1
+			f.CFrame = hrp.CFrame
+		end
+	end
+	i = stop + 1
+	if i <= #food then
+		task.delay(0.1, move)
+	end
 end
-yess()
+move()
